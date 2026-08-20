83fb39f4f7a8e5968e1b18028a8904038808f6a5
Anna Tropina <anna.tropina.1026@yandex.ru>
2026-08-20 14:21:07 +0300

    junit4 commit

--- src/test/java/ru/netology/service/CashBackHackerServiceTest.java
+++ src/test/java/ru/netology/service/CashBackHackerServiceTest.java
@@ -1,28 +1,26 @@
 package ru.netology.service;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.Assert.*;
 
-class CashBackHackerServiceTest {
+public class CashBackHackerServiceTest {
 
     @Test
-    void shouldRemain100() {
+    public void shouldRemain100() {
         CashBackHackerService service = new CashBackHackerService();
         int amount = 900;
         int expected = 100;
         int actual = service.remain(amount);
         assertEquals(expected, actual);
-
     }
 
     @Test
-    void shouldRemainWhenAmountIs1000() {
+    public void shouldRemainWhenAmountIs1000() {
         CashBackHackerService service = new CashBackHackerService();
         int amount = 1000;
         int expected = 0;
         int actual = service.remain(amount);
         assertEquals(expected, actual);
-
     }
 }
